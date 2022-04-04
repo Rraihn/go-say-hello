56f9307171f3991d979e063073c614c7b8f0b9e8
Rraihan <dicaspindo@gmail.com>
2022-04-04 21:33:29 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Rraihn/go-say-hello
+
+go 1.18
---
+++ say_hello.go
@@ -0,0 +1,9 @@
+package belajar_golang_gitfhub
+
+func SayHello(nama string) string {
+	return "Hello " + nama
+}
+
+func Kali(number1 int, number2 int) int {
+	return number1 * number2
+}
